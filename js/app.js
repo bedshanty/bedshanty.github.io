c551f809cb3d80a712e74ae5121d3298fa73bc5f
bedshanty <bedshanty@gmail.com>
2025-09-20 22:36:12 +0900

    계산서 UI 추가

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
+
+// elements
 const shakeElements = document.getElementsByClassName("shake");
 const flickerContainer = document.getElementById("flicker-container");
 const tvFlicker = document.getElementById("tv-flicker");
+const checkContainer = document.getElementById("ui-check-container");
 
+// animation values
 let lastTime = 0;
 const interval = 1000 / 15;
 const backgroundFlickerIntensity = 0.015;
 const backgroundShakeIntensity = 1;
 const tvFlickerIntensity = 0.2;
+
+const onCheckClick = () => {
+  checkContainer.classList.toggle("active");
+}
 
 const animate = (currentTime) => {
   if (!lastTime) {
