458d0b789df3ee28caba0459e686696df6ef2062
bedshanty <bedshanty@gmail.com>
2025-09-27 07:28:46 +0900

    와인병 추가

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // endpoints
 const endpoint = "https://bartender-api-674630912636.asia-northeast3.run.app";
 const getNowUrl = endpoint + "/now";
-const getLatestRecordUrl = endpoint + "/record/latest"
-const postTaskUrl = endpoint + "/task"
+const getLatestRecordUrl = endpoint + "/record/latest";
+const postTaskUrl = endpoint + "/task";
 
 // elements - background
 const frameArtElement = document.getElementById("frame-art");
@@ -26,6 +26,9 @@ const recordTotalTextElement = document.getElementById("ui-check-content-total")
 // elements - bartender
 const bartenderContainerElement = document.getElementById("bartender-container");
 
+// elements - bottle overlays
+const overlayElements = document.getElementsByClassName("overlay");
+
 // animation values
 let lastTime = 0;
 const interval = 1000 / 15;
@@ -34,10 +37,16 @@ const backgroundShakeIntensity = 1;
 const tvFlickerIntensity = 0.2;
 
 // task item constants
-const machugiId = "machugi"
-const machugiImageUrl = "/img/icon_machugi.png"
-const stickmanId = "stickman"
-const stickmanImageUrl = "/img/icon_stickman.png"
+const machugiId = "machugi";
+const machugiImageUrl = "/img/icon_machugi.png";
+const stickmanId = "stickman";
+const stickmanImageUrl = "/img/icon_stickman.png";
+
+// bottle overlay constants
+const defaultBottleId = "default";
+const newspaperBottleId = "newspaper";
+const activeOverlayClassName = "overlay-active";
+const overlayIdPrefix = "overlay-";
 
 // api key
 const apiKeyQueryParameter = "key";
@@ -118,6 +127,24 @@ const onBartenderDragStart = async () => {
   return requestPostTask(null);
 }
 
+const onDefaultBottomClick = () => {
+  onBottleClick(defaultBottleId);
+};
+
+const onNewspaperBottomClick = () => {
+  onBottleClick(newspaperBottleId);
+};
+
+const onBottleClick = (bottomId) => {
+  for (const overlay of overlayElements) {
+    if (overlay.id === overlayIdPrefix + bottomId) {
+      overlay.classList.add(activeOverlayClassName);
+    } else {
+      overlay.classList.remove(activeOverlayClassName);
+    }
+  }
+}
+
 const requestPostTask = async (itemId) => {
   if (apiKey == null) {
     return;
@@ -143,7 +170,7 @@ const requestPostTask = async (itemId) => {
 }
 
 const convertTimeToCost = (time) => {
-  return `€ ${Math.floor(time / 3600)},${Math.floor(time % 3600 / 60).toString().padStart(2, "0")}`
+  return `€ ${Math.floor(time / 3600)},${Math.floor(time % 3600 / 60).toString().padStart(2, "0")}`;
 }
 
 const getImageUrl = (key) => {
